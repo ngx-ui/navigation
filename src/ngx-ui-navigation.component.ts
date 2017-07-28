f60a54d8177a1c2b1e59e488de25500b9ec4bbcb
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-28 16:04:37 +0200

    refactor: change access to navigation property from service by using get method, change navigation property type of component with add undefined

--- src/ngx-ui-navigation.component.ts
+++ src/ngx-ui-navigation.component.ts
@@ -20,30 +20,36 @@ import template from './ngx-ui-navigation.component.html';
 export class NavigationComponent implements OnInit {
   @Input() name: string;
 
-  NavigationService: NavigationService;
-  navigation: Object;
+  /**
+   * Get navigation with inputted name.
+   * @type {(Object | undefined)}
+   * @memberof NavigationComponent
+   */
+  navigation: Object | undefined;
 
   /**
    * Creates an instance of NavigationComponent.
-   * @param {NavigationService} NavigationService 
-   * @param {Router} router 
+   * @param {NavigationService} NavigationService
+   * @param {Router} router
    * @memberof NavigationComponent
    */
-  constructor(NavigationService: NavigationService, private router: Router) {
-    this.NavigationService = NavigationService;
-  }
+  constructor(
+    private navigationService: NavigationService,
+    private router: Router
+  ) { }
 
   /**
    * @memberof NavigationComponent
    */
   ngOnInit() {
     if (this.name) {
-      this.navigation = this.NavigationService.navigation[this.name];
+      this.navigation = this.navigationService.get(this.name);
 
+      // router events
       this.router.events.subscribe(
         (value) => {
           if (value instanceof NavigationEnd) {
-            this.NavigationService.selectByUrl(this.name, value.url);
+            this.navigationService.selectByUrl(this.name, value.url);
           }
         }
       );
